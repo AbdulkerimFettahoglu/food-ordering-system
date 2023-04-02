fa40db46bcb51803a92b4c5831bdde8452e4f9e1
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2023-04-02 17:31:05 +0300

    Customer and Restaurant aggregate root classes added.

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/kerimfettahoglu/food/ordering/system/domain/entity/Customer.java
@@ -0,0 +1,6 @@
+package com.kerimfettahoglu.food.ordering.system.domain.entity;
+
+import com.kerimfettahoglu.food.ordering.system.domain.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+}
---
+++ order-service/order-domain/order-domain-core/src/main/java/com/kerimfettahoglu/food/ordering/system/domain/entity/Restaurant.java
@@ -0,0 +1,14 @@
+package com.kerimfettahoglu.food.ordering.system.domain.entity;
+
+import com.kerimfettahoglu.food.ordering.system.domain.valueobject.RestaurantId;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class Restaurant extends AggregateRoot<RestaurantId> {
+    private final List<Product> product;
+    private boolean active;
+}
