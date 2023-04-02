22b43aff94f5bfe6407419d5036d573252299ba3
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2023-04-02 14:10:03 +0300

    generic zero money object created.

--- common/common-domain/src/main/java/com/kerimfettahoglu/food/ordering/system/domain/valueobject/Money.java
+++ common/common-domain/src/main/java/com/kerimfettahoglu/food/ordering/system/domain/valueobject/Money.java
@@ -14,6 +14,7 @@ import java.math.RoundingMode;
 @Getter
 @EqualsAndHashCode
 public class Money {
+    public static final Money ZERO = new Money(BigDecimal.ZERO);
     private final BigDecimal amount;
 
     public boolean isGreaterThanZero() {
